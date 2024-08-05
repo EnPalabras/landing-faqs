3d8215a19d596f4bfe7acb91c02647aa191a3751
Agustin Iglesias <agusiglesias72@gmail.com>
2024-08-05 17:30:10 -0300

    add favicon and stuff

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import Hero from './components/Hero/Hero'
+import FirstSections from './components/Sections/FirstSections'
+import Navbar from './components/Nav/Nav'
+import Image from 'next/image'
+import logo from '../../public/assets/logo.webp'
+
+export default function Home() {
+  return (
+    <>
+      {/* <Navbar /> */}
+      <header>
+        {' '}
+        <Image src={logo.src} alt="En Palabras" width={303} height={90} />
+      </header>
+
+      <Hero />
+      <main className="container px-4">
+        <FirstSections />
+      </main>
+    </>
+  )
+}
